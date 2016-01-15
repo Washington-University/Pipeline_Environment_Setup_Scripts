8925931f7c323b03e09a5ea8024bc8c2c0ec1239
HCP Pipeline Account <HCPpipeline@login02.cluster>
2016-01-15 16:56:06 -0600

    Modified epd-python_setup.sh for Cluster 2.0

--- epd-python_setup.sh
+++ epd-python_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-EPD_PYTHON_HOME=/export/epd-7.3.2
+if [[ "${CLUSTER}" == "2.0" ]] ; then
+    EPD_PYTHON_HOME=${HOME}/export/epd-7.3.2
+else
+    EPD_PYTHON_HOME=/export/epd-7.3.2
+fi
+
 PATH=${EPD_PYTHON_HOME}/bin:${PATH}
 export PATH
