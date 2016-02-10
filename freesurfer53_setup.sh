371f1555f7307a3c352a0a1a1dc07ee502b6e370
Timothy B. Brown <tbbrown@wustl.edu>
2016-02-10 16:59:34 -0600

    Modifications to freesurfer53_setup.sh for cluster 2.0

--- freesurfer53_setup.sh
+++ freesurfer53_setup.sh
@@ -4,7 +4,7 @@ if [[ "${CLUSTER}" == "1.0" ]]; then
     PACKAGES_DIR=/export/freesurfer-5.3-HCP
 
     if [ -z "$FSLDIR" ]; then
-	source /home/HCPpipeline/SCRIPTS/fsl5_setup.sh
+		source /home/HCPpipeline/SCRIPTS/fsl5_setup.sh
     fi
     source ${PACKAGES_DIR}/SetUpFreeSurfer.sh
     QA_SCRIPTS=$FREESURFER_HOME/QAtools
@@ -12,20 +12,26 @@ if [[ "${CLUSTER}" == "1.0" ]]; then
     PATH=$RECON_CHECKER_SCRIPTS:$QA_SCRIPTS:$PATH
     export PATH
 
-elif
-    PACKAGES_DIR=/act/freesurfer-5.3.0-HCP
+elif [[ "${CLUSTER}" == "2.0" ]]; then
 
-    if [ -z "$FSLDIR" ]; then
-	source /home/HCPpipeline/SCRIPTS/fsl5_setup.sh
-    fi
-    source ${PACKAGES_DIR}/SetUpFreeSurfer.sh
-    QA_SCRIPTS=$FREESURFER_HOME/QAtools
-    RECON_CHECKER_SCRIPTS=$QA_SCRIPTS/data_checker
-    PATH=$RECON_CHECKER_SCRIPTS:$QA_SCRIPTS:$PATH
-    export PATH
+	export FSL_DIR="${FSLDIR}"
+	export FREESURFER_HOME=/act/freesurfer-5.3.0-HCP
+	source ${FREESURFER_HOME}/SetUpFreeSurfer.sh
+
+
+#    PACKAGES_DIR=/act/freesurfer-5.3.0-HCP
+#
+#    if [ -z "$FSLDIR" ]; then
+#		source /home/HCPpipeline/SCRIPTS/fsl5_setup.sh
+#    fi
+#    source ${PACKAGES_DIR}/SetUpFreeSurfer.sh
+#    QA_SCRIPTS=$FREESURFER_HOME/QAtools
+#    RECON_CHECKER_SCRIPTS=$QA_SCRIPTS/data_checker
+#    PATH=$RECON_CHECKER_SCRIPTS:$QA_SCRIPTS:$PATH
+#    export PATH
     
 else
     echo "freesurfer53_setup.sh: -- ERROR -- Unable to CLUSTER: '${CLUSTER}' value to determine location of FreeSurfer 5.3 HCP"
-
+	
 fi
 
