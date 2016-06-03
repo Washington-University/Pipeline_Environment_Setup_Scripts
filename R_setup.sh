95c2d2c83624b75e1613a64c4745c57bd57ed7e7
Timothy B. Brown <tbbrown@wustl.edu>
2016-06-03 10:39:36 -0500

    Modified R_setup.sh to handle CHPC cluster 2.0

--- R_setup.sh
+++ R_setup.sh
@@ -5,9 +5,13 @@ if [[ "${CLUSTER}" == "1.0" ]]; then
 	PATH=${R_HOME}/bin:${PATH}
 	export R_HOME PATH
 
+	echo "R_setup.sh: Set up to run R from ${R_HOME}"
+
 elif [[ "${CLUSTER}" == "2.0" ]]; then
-	export R_HOME=/act/R-3.2.1
+	export R_HOME=${HOME}/R-3.0.2
 	export PATH=${R_HOME}/bin:${PATH}
+
+	echo "R_setup.sh: Set up to run R from ${R_HOME}"
 
 else
 	echo "R_setup.sh: -- ERROR -- Unable to use CLUSTER: '${CLUSTER}' value to determine location of R"
